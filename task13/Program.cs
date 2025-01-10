79257cdef4831f64e5ab2f1418e55ef1a4d82361
XetayiMemmedov <khatayirm@code.edu.az>
2025-01-10 16:32:54 +0400

    Add project files.

---
+++ task13/Program.cs
@@ -0,0 +1,49 @@
+﻿//13cu tapsirigin helli
+Console.WriteLine("Saniyeni daxil edin:");
+int s=int.Parse(Console.ReadLine());
+//int s = 31600000;
+double qaliq = 0;
+double d = 0;
+double h = 0;
+double m = 0;
+double temp = s;
+for (double i = 0; temp >= qaliq; i++)
+{
+    qaliq = Math.Pow(60, 4);
+    if (temp < qaliq)
+        break;
+    temp = temp - qaliq;
+    d++;
+}
+qaliq = 0;
+for (double i = 0; temp >= qaliq; i++)
+{
+    qaliq = Math.Pow(60, 3);
+    if (temp < qaliq)
+        break;
+    temp = temp - qaliq;
+    h++;
+}
+qaliq = 0;
+for (double i = 0; temp >= qaliq; i++)
+{
+    qaliq = Math.Pow(60, 2);
+    if (temp < qaliq)
+        break;
+    temp = temp - qaliq;
+    m++;
+}
+qaliq = 0;
+for (double i = 0; temp >= qaliq; i++)
+{
+    qaliq = Math.Pow(60, 1);
+    if (temp < qaliq)
+        break;
+    temp = temp - qaliq;
+    h++;
+}
+Console.Write($"{s} saniye - {d} gun, {h} saat, {m} deqiqe, {temp} saniyedir ");
+
+
+
+
