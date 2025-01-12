c06dd8d8e6fa5dbae6f2bd3df061cc5aac37a727
XetayiMemmedov <khatayirm@code.edu.az>
2025-01-12 17:36:17 +0400

    last correction

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,7 +1,6 @@
 ﻿//13cu tapsirigin helli
 Console.WriteLine("Saniyeni daxil edin:");
-int s=int.Parse(Console.ReadLine());
-//int s = 31600000;
+int s = int.Parse(Console.ReadLine());
 double qaliq = 0;
 double d = 0;
 double h = 0;
@@ -9,7 +8,7 @@ double m = 0;
 double temp = s;
 for (double i = 0; temp >= qaliq; i++)
 {
-    qaliq = Math.Pow(60, 4);
+    qaliq = 24 * Math.Pow(60, 3);
     if (temp < qaliq)
         break;
     temp = temp - qaliq;
@@ -18,20 +17,11 @@ for (double i = 0; temp >= qaliq; i++)
 qaliq = 0;
 for (double i = 0; temp >= qaliq; i++)
 {
-    qaliq = Math.Pow(60, 3);
-    if (temp < qaliq)
-        break;
-    temp = temp - qaliq;
-    h++;
-}
-qaliq = 0;
-for (double i = 0; temp >= qaliq; i++)
-{
     qaliq = Math.Pow(60, 2);
     if (temp < qaliq)
         break;
     temp = temp - qaliq;
-    m++;
+    h++;
 }
 qaliq = 0;
 for (double i = 0; temp >= qaliq; i++)
@@ -40,9 +30,17 @@ for (double i = 0; temp >= qaliq; i++)
     if (temp < qaliq)
         break;
     temp = temp - qaliq;
-    h++;
+    m++;
 }
-Console.Write($"{s} saniye - {d} gun, {h} saat, {m} deqiqe, {temp} saniyedir ");
+qaliq = 0;
+if (d!=0)
+    Console.Write($"{s} saniye - {d} gun, {h} saat, {m} deqiqe, {temp} saniyedir ");
+else if (d==0 && h!=0)
+    Console.Write($"{s} saniye - {h} saat, {m} deqiqe, {temp} saniyedir ");
+else if (d==0 && h == 0 && m != 0)
+    Console.Write($"{s} saniye - {m} deqiqe, {temp} saniyedir ");
+else
+    Console.Write($"{s} saniye - {temp} saniyedir ");
 
 
 
